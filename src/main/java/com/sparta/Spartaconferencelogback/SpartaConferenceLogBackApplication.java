767b2b6baacee627749f19e6d217dc2c4b8d9c59
salmon2 <dys04076@naver.com>
2021-10-11 18:14:07 +0900

    first commit

---
+++ src/main/java/com/sparta/Spartaconferencelogback/SpartaConferenceLogBackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.Spartaconferencelogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaConferenceLogBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaConferenceLogBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/Spartaconferencelogback/SpartaConferenceLogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.Spartaconferencelogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaConferenceLogBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
